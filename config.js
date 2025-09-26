08415c8666bce61ccf7caf34fbffb46e94bd61ec
nesco177 <abdullah.nesco@gmail.com>
2025-09-26 19:26:24 +0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-
-window.FIREBASE_API_KEY = 'AIzaSyAy53-F5QZ5a0f9DW9emesit1KA2T7v7Do';
-window.FIREBASE_AUTH_DOMAIN = 'testt-b241d.firebaseapp.com';
-window.FIREBASE_PROJECT_ID = 'testt-b241d';
-window.FIREBASE_STORAGE_BUCKET = 'testt-b241d.appspot.com';
-window.FIREBASE_MESSAGING_SENDER_ID = '288476078303';
-window.FIREBASE_APP_ID = '1:288476078303:android:ff06d4a9d5c432f41ac96a';
